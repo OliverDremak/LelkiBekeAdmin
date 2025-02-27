372f6239c68377a4bc36d22f8d035c944b32f8b2
Oliver <Oliver@DESKTOP-PAT845E>
2025-02-27 12:53:35 +0100

    1

---
+++ LelkiBekeAdmin/Storage/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LelkiBekeAdmin.Storage
+{
+    public static class TokenService
+    {
+        private const string TokenKey = "auth:token";
+
+        public static async Task SaveTokenAsync(string token)
+        {
+            await SecureStorage.SetAsync(TokenKey, token);
+        }
+
+        public static async Task<string> GetTokenAsync()
+        {
+            return await SecureStorage.GetAsync(TokenKey);
+        }
+
+        public static async Task DeleteTokenAsync()
+        {
+            await SecureStorage.SetAsync(TokenKey, string.Empty);
+        }   
+    }
+}
